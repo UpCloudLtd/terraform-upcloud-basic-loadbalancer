551f016ac94d90e25b19cd1307655cac4897ef2b
Toni Kangas <toni.kangas@upcloud.com>
2022-06-02 14:03:37 +0300

    Make the basic example easier to deploy and test

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -9,8 +9,12 @@ terraform {
 
 provider "upcloud" {}
 
+locals {
+  prefix = "lb-module-basic-example-"
+}
+
 resource "upcloud_network" "network" {
-  name = "my_network"
+  name = "${local.prefix}net"
   zone = "pl-waw1"
 
   ip_network {
@@ -22,14 +26,24 @@ resource "upcloud_network" "network" {
 
 resource "upcloud_server" "webservers" {
   count    = 3
-  hostname = "webserver${count.index}"
-  title    = "webserver_${count.index}"
+  hostname = "${local.prefix}server-${count.index}"
+  title    = "${local.prefix}server-${count.index}"
   zone     = "pl-waw1"
-  plan     = "1xCPU-2GB"
+  plan     = "1xCPU-1GB"
+
+  # Use user-data script to install and launch http server on start
+  metadata  = true
+  user_data = file("${path.module}/user-data-script.sh")
 
   template {
     storage = "Ubuntu Server 20.04 LTS (Focal Fossa)"
     size    = 25
+    title   = "${local.prefix}storage-${count.index}"
+  }
+
+  # Required to have internet access when installing httpd on launch
+  network_interface {
+    type = "public"
   }
 
   network_interface {
@@ -45,12 +59,18 @@ resource "upcloud_server" "webservers" {
 module "load_balancer" {
   source = "UpCloudLtd/basic-loadbalancer/upcloud"
 
-  name                = "my_loadbalancer"
+  name                = "${local.prefix}lb"
   zone                = "pl-waw1"
   network             = upcloud_network.network.id
-  backend_servers     = [for v in upcloud_server.webservers : v.network_interface.1.ip_address]
-  backend_server_port = 3000
-  domains             = ["my.domain.net"]
+  backend_servers     = [for v in upcloud_server.webservers : v.network_interface.2.ip_address]
+  backend_server_port = 80
+
+  # To enable TLS on the frontend:
+  # - Uncomment and replace example.com with your domain
+  # - Change frontend port from 80 to 443
+
+  # domains       = ["example.com"]
+  frontend_port = 80
 }
 
 output "lb_url" {
